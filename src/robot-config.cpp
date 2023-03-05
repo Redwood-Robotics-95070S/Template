0009540239ff41ccbfde229112190da50b120ddd
VY <amzingfood@duck.com>
2023-03-05 12:16:04 -0800

    ports

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -10,9 +10,9 @@ brain  Brain;
 // VEXcode device constructors
 controller Controller1 = controller(primary);
 motor fl = motor(PORT1, ratio18_1, false);
-motor fr = motor(PORT2, ratio18_1, true);
-motor bl = motor(PORT3, ratio18_1, false);
-motor br = motor(PORT4, ratio18_1, true);
+motor fr = motor(PORT10, ratio18_1, true);
+motor bl = motor(PORT10, ratio18_1, false);
+motor br = motor(PORT3, ratio18_1, true);
 motor roller = motor(PORT9, ratio18_1, false);
 gps GPS = gps(PORT6, 0.00, 0.00, mm, 180);
 inertial Inertial = inertial(PORT7);
