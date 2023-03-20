46c051b21fef39f0194d720657a192239dd62025
VY <122563505+Astlantix@users.noreply.github.com>
2023-03-20 13:46:05 -0700

    Update robot-config.h

--- include/robot-config.h
+++ include/robot-config.h
@@ -2,26 +2,14 @@ using namespace vex;
 
 extern brain Brain;
 
-using signature = vision::signature;
-
 // VEXcode devices
 extern controller Controller1;
 extern motor fl;
 extern motor fr;
 extern motor bl;
 extern motor br;
-extern motor roller;
-extern gps GPS;
-extern inertial Inertial21;
-extern optical Optical19;
-extern signature Vision20__SIG_1;
-extern signature Vision20__SIG_2;
-extern signature Vision20__SIG_3;
-extern signature Vision20__SIG_4;
-extern signature Vision20__SIG_5;
-extern signature Vision20__SIG_6;
-extern signature Vision20__SIG_7;
-extern vision Vision20;
+extern gps GPST;
+extern inertial Inertial;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
